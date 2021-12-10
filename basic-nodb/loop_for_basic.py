111bfddb024beb3f4ad936441ff3f72fbfbb398c
Agung Zon Blade <53517318+zonblade@users.noreply.github.com>
2021-12-10 23:52:13 +0700

    loop for basic

---
+++ basic-nodb/loop_for_basic.py
@@ -0,0 +1,11 @@
+# looping list
+# tanpa index
+text = list('abcdef')
+for val in text:
+	print(val)
+
+# looping list
+# dengan index
+text = list('abcdef')
+for (index,value) in enumerate(text):
+	print(index)
