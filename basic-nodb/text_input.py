6d6171185eecccd7ae5915a3cca2901bdb84c7ad
Agung Zon Blade <53517318+zonblade@users.noreply.github.com>
2021-12-10 22:47:26 +0700

    input teks

---
+++ basic-nodb/text_input.py
@@ -0,0 +1,7 @@
+# menggunakan fitur input
+# didalam input() bisa ditambahkan
+# teks untuk instruksi.
+teks = input('input apapun: ')
+print(f"hasil input: {teks}")
+
+# hasil input: apapun yang di input.
