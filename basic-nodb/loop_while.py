67c8aebe0675604111a7fe0be8de4bc17084525a
Agung Zon Blade <53517318+zonblade@users.noreply.github.com>
2021-12-10 23:51:40 +0700

    loop while basic

---
+++ basic-nodb/loop_while.py
@@ -0,0 +1,14 @@
+# harus memahami < <= > >= != ==
+# loop while dengan parameter terdefinisi
+x = 1
+while x < 10:
+	print(x)
+	x = x+1
+
+# loop while dengan banyaknya huruf
+# text dapat berupa list maupun string
+text = 'abcdef'
+x = 0
+while x < len(text):
+	print(x)
+	x = x+1
