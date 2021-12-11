47a7c49d28863f40aa615031a6454689679c8de0
Agung Zon Blade <53517318+zonblade@users.noreply.github.com>
2021-12-11 13:59:44 +0700

    mencari average/rata-rata basic

---
+++ basic-nodb/average_rata-rata.py
@@ -0,0 +1,12 @@
+# mendefiniskan fungsi average
+def average(angka):
+  # menjumlahkan angka dalam list
+  # dan membaginya dengan banyaknya angka dalam list
+  return sum(angka)/len(angka)
+
+# penggunaan
+angka = [3,4,5,6,3,2,1,6,5]
+hasil = average(angka)
+print(hasil)
+
+# versi advanced average akan ku update.
